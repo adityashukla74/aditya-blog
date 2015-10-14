ca59dd47f75d29a4d65ecbfd0a38caaec8b73a81
aditya shukla <adityashukla74@gmail.com>
2015-10-14 21:44:49 +0530

    [CHORE] Routing Successful

--- aditya-blog/test/controllers/static_pages_controller_test.rb
+++ aditya-blog/test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+	@base_title = "RoR Sample App"
+  end  
   test "should get home" do
     get :home 
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
   test "should get about" do
    get :about
    assert_response :success
+   assert_select "title", "About | #{@base_title}" 
   end
 end
+
