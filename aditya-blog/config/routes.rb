208263ff431097f92ad94d731405f38bbab4efa7
aditya shukla <adityashukla74@gmail.com>
2015-10-15 17:50:15 +0530

    [CHORE] NOT ABLE TO SORT ROUTING ISSUE, MOVING TO OTHER PARTS, WILL SORT ROUTING LATER

--- aditya-blog/config/routes.rb
+++ aditya-blog/config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'static_pages#home'
-  get  'static_pages/contact'
-  get  'static_pages/about' 
+    root             'static_pages#home'
+    get 'about' => 'static_pages#about'
  # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
