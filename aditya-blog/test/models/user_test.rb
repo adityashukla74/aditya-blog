3b8e52c3efc9643d721d9dd8291c0c747a9c37f5
aditya shukla <adityashukla74@gmail.com>
2015-10-15 18:20:58 +0530

    [CHORE] USER VALIDATION TEST FOR LOGIN

---
+++ aditya-blog/test/models/user_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+	@user = User.new(name: "Aditya User",email: "adityashukla74@gmail.com")
+  end
+  
+  test "should be valid" do
+  assert @user.valid?
+  end
+end
